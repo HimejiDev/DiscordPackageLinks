fe4e9322fc3edec6a8706cc7f380fd5b2bd5479d
Himeji#1978 <88136704+HimejiDev@users.noreply.github.com>
2023-02-19 15:53:00 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const input = document.getElementById("directory");
 const filesContainer = document.getElementsByClassName("filesContainer")[0];
 const _status = document.getElementsByClassName("status")[0];
+const pageContainer = document.getElementsByClassName('pages')[0];
+
+let files = []
+let fileDisplayAmounts = []
+let currentPage = 0;
+const filesPage = 1000;
 
 input.onchange = () => {
     _status.innerHTML = 'Status: Checking Messages';
-    filesContainer.innerHTML = '';
+    pageContainer.innerHTML = '';
 
     const worker = new Worker('worker.js');
 
@@ -17,25 +23,58 @@ input.onchange = () => {
         let totalDone = 0;
 
         messages.forEach((message, index) => {
-            setTimeout(() => {
-                displayImg(message);
+            addFile(message);
 
-                totalDone++;
+            totalDone++;
 
-                if (totalDone === totalFiles) {
-                    _status.innerHTML = `Status: ${calculatePro(totalDone, totalFiles)} ${totalDone}/${totalFiles}`;
-                } else {
-                    _status.innerHTML = `Status: ${calculatePro(totalDone, totalFiles)}% ${totalDone}/${totalFiles} (loading...)`;
+            if (totalDone === totalFiles) {
+                _status.innerHTML = `Status: ${calculatePro(totalDone, totalFiles)}% ${totalDone}/${totalFiles} [Done]`;
+                
+                for (let i = 0; i < Math.floor(files.length/filesPage); i++) {
+                    fileDisplayAmounts.push(filesPage);
+                    pageContainer.innerHTML += `<div class="page" id="${i+1}">${i+1}</div>`
                 }
-            }, index * 10);
+                if (files.length%filesPage > 0) {
+                    fileDisplayAmounts.push(files.length%filesPage);
+                    pageContainer.innerHTML += `<div class="page" id="${fileDisplayAmounts.length}">${fileDisplayAmounts.length}</div>`
+                }
+
+                const pages = document.getElementsByClassName("page");
+                for (let i = 0; i < pages.length; i++) {
+                    pages[i].onclick = function () {
+                        DisplayPage(pages[i]);
+                    };
+                }
+
+                DisplayFilesIndex(files, fileDisplayAmounts[currentPage], addInt(fileDisplayAmounts, 0, currentPage-1));
+            } else {
+                _status.innerHTML = `Status: ${calculatePro(totalDone, totalFiles)}% ${totalDone}/${totalFiles} [loading${totalDone % 4}]`;
+            }
         });
     };
 };
 
 availbleExtentions = ['mp4', 'webm', 'ogg', 'mov', 'png', 'jpg', 'jpeg']
-function displayImg(message) {
+function addFile(message) {
     const extension = message.split('.').pop().toLowerCase();
     if (availbleExtentions.includes(extension)) {
+        files.push(message);
+    }
+}
+
+function calculatePro(part, whole) {
+    return Math.round((part / whole) * 100);
+}
+
+function DisplayFilesIndex(array, amount, skip) {
+    console.log(amount, skip)
+    console.log(fileDisplayAmounts);
+    filesContainer.innerHTML = '';
+    filesContainer.children = '';
+    filesContainer.childNodes = '';
+    for (let i = 0; i < amount; i++) {
+        const message = array[i + skip];
+        const extension = message.split('.').pop().toLowerCase();
         let element;
         if (extension === 'mp4' || extension === 'webm' || extension === 'ogg' || extension === 'mov') {
             element = document.createElement('video');
@@ -61,8 +100,24 @@ function displayImg(message) {
         fileContainer.className = 'fileContainer'
         filesContainer.appendChild(fileContainer);
     }
+    _status.innerHTML = `Status: Shown ${amount}/${array.length}`;
 }
 
-function calculatePro(part, whole) {
-    return Math.round((part / whole) * 100);
+function DisplayPage(page) {
+    console.log(page.attributes.id.nodeValue)
+    currentPage = page.attributes.id.nodeValue - 1;
+    DisplayFilesIndex(files, fileDisplayAmounts[currentPage], addInt(fileDisplayAmounts, 0, currentPage-1));
+}
+
+function addInt(array, start, end) {
+    console.log(start, end);
+    console.log(end >= 0)
+    if (end >= 0) {
+        var result = 0;
+        for (let i = 0; i < end+1; i++) {
+            result += array[i + start];
+        }
+        return result;
+    } else
+        return 0;
 }
